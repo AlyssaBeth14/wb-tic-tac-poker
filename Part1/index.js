6c5207d7d1a758b32dc3499c4fed6b78aa07e83f
Alyssa Burdett <alyssa.burdett14@gmail.com>
2023-11-09 15:47:48 -0700

    second commit

---
+++ Part1/index.js
@@ -0,0 +1,62 @@
+
+let player = 'X'
+
+function play(evt) {
+    const evtTarget = evt.target
+    evtTarget.innerText = player
+    if(player === 'X'){
+        player = 'O'
+    } else {
+        player = 'X'
+    }  
+const playerTurn = document.querySelector('#current-player')
+playerTurn.innerText = player
+
+const winner = calculateWinner()
+if(winner){
+    alert(`Player ${winner} won!`)
+  } else if (isBoardFull()){
+    alert('Game is a tie!')
+  }
+}
+
+const squares = document.querySelectorAll('.square')
+
+function calculateWinner(){
+const lines = [
+    // Horizontal lines
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical lines
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal lines
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+    for(const line of lines){
+        const [a, b, c] = line
+        const squareAText = squares[a].innerText
+        const squareBText = squares[b].innerText
+        const squareCText = squares[c].innerText
+        if (squareAText !== '' && squareAText === squareBText && squareAText === squareCText){
+            return squareAText
+        }
+    }
+    return undefined
+  }
+
+for(const square of squares){
+    square.addEventListener('click', play)
+}
+
+function isBoardFull() {
+    for(const square of squares){
+        if(square.innerText === ''){
+            return false
+        }
+    }
+    return true
+}
